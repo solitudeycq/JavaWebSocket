792b87098e807c33dc133a5369517ec9a888505a
solitudeycq <solitudeycq@163.com>
2017-10-02 00:47:35 +0800

    chapter 1,and add class EchoServer.

---
+++ src/chapter1/EchoServer.java
@@ -0,0 +1,22 @@
+/**
+ * package_name:chapter1
+ * @author:solitudeycq
+ * date:2017-10-02 00:44:26
+ * E-Mail:solitudeycq@163.com
+ */
+package chapter1;
+
+import javax.websocket.OnMessage;
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ * @author solitudeycq
+ *
+ */
+@ServerEndpoint("/echo")
+public class EchoServer {
+	@OnMessage
+	public String echo(String incomingMessage) {
+		return "I got this ("+incomingMessage+")"+" so I am sending it back.";
+	}
+}
