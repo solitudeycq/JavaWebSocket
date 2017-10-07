f83031b15c6284aca5dbd189c164c45c63c3f71d
solitudeycq <solitudeycq@163.com>
2017-10-07 20:40:57 +0800

    测试服务器是否能主动发送消息。

--- src/chapter1/ProgrammaticEchoServer.java
+++ src/chapter1/ProgrammaticEchoServer.java
@@ -7,6 +7,10 @@
 package chapter1;
 
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.websocket.Endpoint;
 import javax.websocket.EndpointConfig;
@@ -22,7 +26,21 @@ public class ProgrammaticEchoServer extends Endpoint {
 	@Override
 	public void onOpen(Session session, EndpointConfig endpointConfig) {
 		System.out.println("执行了");
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.SECOND, calendar.get(Calendar.SECOND)+3);
 		final Session mySession = session;
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			
+			@Override
+			public void run() {
+				try {
+					mySession.getBasicRemote().sendText((new Date()).toString());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}, calendar.getTime(),2000);
 		mySession.addMessageHandler(new MessageHandler.Whole<String>() {
 
 			@Override
